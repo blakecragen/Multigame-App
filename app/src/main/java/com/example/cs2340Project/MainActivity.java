ba1afcdc92f71252862934ce178f410a260e5a76
blakecragen <113573797+blakecragen@users.noreply.github.com>
2023-05-31 11:07:17 -0400

    Classes and buttons have been added to transition into the "Start screen" of the wordle game. The .xml is the layout/preview of the wordle start screen. The wordleStartScreen class describes the behavior of the class.
    Note: buttons have been added to both the home screen and the wordle home screen. However, only the homescreen to wordleStartScreen has an implementation. The button still has to be created for wordleStartScreen to homescreen (MainActivity).

--- app/src/main/java/com/example/cs2340Project/MainActivity.java
+++ app/src/main/java/com/example/cs2340Project/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.cs2340Project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button wordleScreenButton = (Button) findViewById(R.id.homeToWordleStart);
+        wordleScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWordleStartScreen();
+            }
+        });
+    }
+
+    public void openWordleStartScreen() {
+        Intent intent = new Intent(this, wordleStartScreen.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/cs2340Project/wordleStartScreen.java
@@ -0,0 +1,14 @@
+package com.example.cs2340Project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class wordleStartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wordle_start_screen);
+    }
+}
