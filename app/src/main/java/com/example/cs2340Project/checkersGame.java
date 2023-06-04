b3b90b8c64cd1cf5addaf366ae2e5aba463ef411
Christeena Joby <christeenamjoby@gmail.com>
2023-06-04 17:56:39 -0400

    Chris/ New Changes

--- app/src/main/java/com/example/cs2340Project/checkersGame.java
+++ app/src/main/java/com/example/cs2340Project/checkersGame.java
@@ -7,12 +7,16 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class checkersGame extends AppCompatActivity {
+    private TextView name;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.checkers_game);
+        name = findViewById(R.id.checkersHeading);
+        String username = getIntent().getStringExtra("keyname");
+        name.setText(username);
 
         Button homeButton = findViewById(R.id.toHome);
         homeButton.setOnClickListener(v -> finish());
--- app/src/main/java/com/example/cs2340Project/checkersInitialScreen.java
+++ app/src/main/java/com/example/cs2340Project/checkersInitialScreen.java
@@ -5,17 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class checkersInitialScreen extends AppCompatActivity {
+    private EditText name;
+    private Button add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.checkers_initial_screen);
+        name = findViewById(R.id.checkersPLayerName);
+        add = findViewById(R.id.gameStart);
 
         Button initButton = findViewById(R.id.gameStart);
         initButton.setOnClickListener(v -> {
             Intent intent = new Intent(checkersInitialScreen.this, checkersGame.class);
+            String username = name.getText().toString();
+            intent.putExtra("keyname", username);
             startActivity(intent);
             finish();
         });
