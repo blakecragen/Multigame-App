7ddcf4fc714dd4bca15c6cb246ee0ab6075d152b
Christeena Joby <christeenamjoby@gmail.com>
2023-06-04 21:30:52 -0400

    Chris/ New Changes

--- app/src/main/java/com/example/cs2340Project/wordleGame.java
+++ app/src/main/java/com/example/cs2340Project/wordleGame.java
@@ -7,12 +7,14 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class wordleGame extends AppCompatActivity {
-
+    private TextView name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wordle_game);
-
+        name = findViewById(R.id.wordleHeading);
+        String username = getIntent().getStringExtra("keyname");
+        name.setText(username);
         Button toHome = findViewById(R.id.toMainActivity);
         toHome.setOnClickListener(v -> finish());
     }
