27065de6cc9f4faf39314b753bb016b4a96dbeac
Blake Cragen <113573797+blakecragen@users.noreply.github.com>
2023-06-10 11:46:00 -0400

    Update wordleGameFunctionality.java
    Beginnings of a class to manage wordle game functionality

--- app/src/main/java/com/example/cs2340Project/wordleGameFunctionality.java
+++ app/src/main/java/com/example/cs2340Project/wordleGameFunctionality.java
@@ -1,3 +1,64 @@
 public class wordleGameFunctionality {
+
+    /** LinkedList-backed/Stack-backed hashmap for finding positions of words. */
+    public Node[] wordHashmap;
+
+    public wordleGameFunctionality() {
+        wordHashmap = new Node[5];
+    }
     
+    /**
+     * Private node class to create a LinkedList-backed stack.
+     */
+    private class Node {
+        private Node next;
+        private char let;
+        private int pos;
+        
+        public Node (int pos, char let, Node next) {
+            this.next = next;
+            this.let = let;
+            this.pos = pos;
+        }
+        
+        public Node (int pos, char let) {
+            this(pos, let, null);
+        }
+    }
+    
+    /**
+     * Method to generate a hashcode/get the ind of a letter in the hashmap.
+     */
+    public int hashcode(char currLet) {
+        currLet = Character.toLowerCase(currLet);
+        int val = ((int) currLet - (int) 'a') % wordHashmap.length;
+        boolean hasBeenFound = false;
+        while (!hasBeenFound) {
+            if (wordHashmap[val] == null || wordHashmap[val].let == currLet) {
+                return val;
+            } else {
+                val = (val + 1) % wordHashmap.length;
+            }
+        }
+        return val;
+    }
+    
+    /**
+     * Method to add the letters of a word to the hashmap.
+     *
+     * @param let Letter getting added.
+     * @param pos Indice/position of the letter in the original String.
+     */
+    public void add(char let, int pos) {
+        int ind = this.hashcode(let);
+        if (wordHashmap[ind] == null) {
+            wordHashmap[ind] = new Node(pos, let);
+        } else {
+            Node last = wordHashmap[ind];
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = new Node(pos, let);
+        }
+    }
 }
