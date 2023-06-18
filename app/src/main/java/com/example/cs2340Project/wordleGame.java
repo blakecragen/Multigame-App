cd0c5d818f297f1a72bcc3fbb734fbe7a17e76bf
Christeena Joby <christeenamjoby@gmail.com>
2023-06-18 15:05:52 -0400

    Chris/ color change

--- app/src/main/java/com/example/cs2340Project/wordleGame.java
+++ app/src/main/java/com/example/cs2340Project/wordleGame.java
@@ -8,9 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class wordleGame extends AppCompatActivity implements View.OnClickListener {
-    private EditText[] editTextFields;
-    private Button enterButton;
-    private Button deleteButton;
+    private EditText[] letterBox;
+    private Button enter;
+    private Button del;
 
     private int letsInRow;
     private wordleGameFunctionality wordle;
@@ -30,37 +30,37 @@ public class wordleGame extends AppCompatActivity implements View.OnClickListene
         Button restart = findViewById(R.id.wd_restart_button);
         restart.setOnClickListener(v -> recreate());
 
-        editTextFields = new EditText[30];
-        editTextFields[0] = findViewById(R.id.editText1);
-        editTextFields[1] = findViewById(R.id.editText2);
-        editTextFields[2] = findViewById(R.id.editText3);
-        editTextFields[3] = findViewById(R.id.editText4);
-        editTextFields[4] = findViewById(R.id.editText5);
-        editTextFields[5] = findViewById(R.id.editText6);
-        editTextFields[6] = findViewById(R.id.editText7);
-        editTextFields[7] = findViewById(R.id.editText8);
-        editTextFields[8] = findViewById(R.id.editText9);
-        editTextFields[9] = findViewById(R.id.editText10);
-        editTextFields[10] = findViewById(R.id.editText11);
-        editTextFields[11] = findViewById(R.id.editText12);
-        editTextFields[12] = findViewById(R.id.editText13);
-        editTextFields[13] = findViewById(R.id.editText14);
-        editTextFields[14] = findViewById(R.id.editText15);
-        editTextFields[15] = findViewById(R.id.editText16);
-        editTextFields[16] = findViewById(R.id.editText17);
-        editTextFields[17] = findViewById(R.id.editText18);
-        editTextFields[18] = findViewById(R.id.editText19);
-        editTextFields[19] = findViewById(R.id.editText20);
-        editTextFields[20] = findViewById(R.id.editText21);
-        editTextFields[21] = findViewById(R.id.editText22);
-        editTextFields[22] = findViewById(R.id.editText23);
-        editTextFields[23] = findViewById(R.id.editText24);
-        editTextFields[24] = findViewById(R.id.editText25);
-        editTextFields[25] = findViewById(R.id.editText26);
-        editTextFields[26] = findViewById(R.id.editText27);
-        editTextFields[27] = findViewById(R.id.editText28);
-        editTextFields[28] = findViewById(R.id.editText29);
-        editTextFields[29] = findViewById(R.id.editText30);
+        letterBox = new EditText[30];
+        letterBox[0] = findViewById(R.id.editText1);
+        letterBox[1] = findViewById(R.id.editText2);
+        letterBox[2] = findViewById(R.id.editText3);
+        letterBox[3] = findViewById(R.id.editText4);
+        letterBox[4] = findViewById(R.id.editText5);
+        letterBox[5] = findViewById(R.id.editText6);
+        letterBox[6] = findViewById(R.id.editText7);
+        letterBox[7] = findViewById(R.id.editText8);
+        letterBox[8] = findViewById(R.id.editText9);
+        letterBox[9] = findViewById(R.id.editText10);
+        letterBox[10] = findViewById(R.id.editText11);
+        letterBox[11] = findViewById(R.id.editText12);
+        letterBox[12] = findViewById(R.id.editText13);
+        letterBox[13] = findViewById(R.id.editText14);
+        letterBox[14] = findViewById(R.id.editText15);
+        letterBox[15] = findViewById(R.id.editText16);
+        letterBox[16] = findViewById(R.id.editText17);
+        letterBox[17] = findViewById(R.id.editText18);
+        letterBox[18] = findViewById(R.id.editText19);
+        letterBox[19] = findViewById(R.id.editText20);
+        letterBox[20] = findViewById(R.id.editText21);
+        letterBox[21] = findViewById(R.id.editText22);
+        letterBox[22] = findViewById(R.id.editText23);
+        letterBox[23] = findViewById(R.id.editText24);
+        letterBox[24] = findViewById(R.id.editText25);
+        letterBox[25] = findViewById(R.id.editText26);
+        letterBox[26] = findViewById(R.id.editText27);
+        letterBox[27] = findViewById(R.id.editText28);
+        letterBox[28] = findViewById(R.id.editText29);
+        letterBox[29] = findViewById(R.id.editText30);
 
 
         int[] buttonIds = {
@@ -71,30 +71,38 @@ public class wordleGame extends AppCompatActivity implements View.OnClickListene
                 R.id.buttonU, R.id.buttonV, R.id.buttonW, R.id.buttonX, R.id.buttonY, R.id.buttonZ
         };
         for (int i = 0; i < buttonIds.length; i++) {
-            Button button = findViewById(buttonIds[i]);
-            button.setOnClickListener(this);
+            Button b = findViewById(buttonIds[i]);
+            b.setOnClickListener(this);
         }
 
-        deleteButton = findViewById(R.id.delete);
-        deleteButton.setOnClickListener(this);
+        del = findViewById(R.id.delete);
+        del.setOnClickListener(this);
 
-        enterButton = findViewById(R.id.enter);
-        enterButton.setOnClickListener(this);
+        enter = findViewById(R.id.enter);
+        enter.setOnClickListener(this);
+    }
+
+    private String buttonLetter() {
+        StringBuilder letter = new StringBuilder();
+        for (int i = 0; i < letsInRow; i++) {
+            letter.append(letterBox[i].getText().toString());
+        }
+        return letter.toString();
     }
 
     public void onClick(View v) {
         if (v.getId() == R.id.delete) {
-            Button deleteButton = (Button) v;
-            String deleteButtonText = deleteButton.getText().toString();
-            if (deleteButtonText.equals("Delete") && letsInRow > 0) {
-                editTextFields[--letsInRow].setText("");
+            Button del = (Button) v;
+            String delText = del.getText().toString();
+            if (delText.equals("Delete") && letsInRow > 0) {
+                letterBox[--letsInRow].setText("");
             } else {
-                for (int i = 0; i < editTextFields.length; i++) {
-                    EditText editText = editTextFields[i];
+                for (int i = 0; i < letterBox.length; i++) {
+                    EditText editText = letterBox[i];
                     if (editText.getText().toString().isEmpty() && letsInRow < 5) {
-                        editText.setText(deleteButtonText);
-                        if (i + 1 < editTextFields.length) {
-                            editTextFields[i + 1].requestFocus();
+                        editText.setText(delText);
+                        if (i + 1 < letterBox.length) {
+                            letterBox[i + 1].requestFocus();
                         }
                         letsInRow++;
                         break;
@@ -102,57 +110,46 @@ public class wordleGame extends AppCompatActivity implements View.OnClickListene
                 }
             }
         } else if (v.getId() == R.id.enter) {
-            Button enterButton = (Button) v;
-            String enterButtonText = enterButton.getText().toString();
-            if (enterButtonText.equals("Enter")) {
+            Button enter = (Button) v;
+            String enterText = enter.getText().toString();
+            if (enterText.equals("Enter")) {
                 if (letsInRow == 5) {
-                    String playerGuess = getRowCharacters();
+                    String playerGuess = buttonLetter();
                     char[] guess = playerGuess.toCharArray();
                     String answer = "MOCHA";
                     char[] solution = answer.toCharArray();
-                    int counter = 0;
+                    int count = 0;
                     for (int i = 0; i < guess.length; i++) {
                         if (guess[i] == solution[i]) {
-                            editTextFields[counter].setBackgroundResource(R.color.green);
+                            letterBox[count].setBackgroundResource(R.color.green);
                         } else if (answer.contains(String.valueOf(guess[i]))) {
-                            editTextFields[counter].setBackgroundResource(R.color.purple);
+                            letterBox[count].setBackgroundResource(R.color.purple);
                         } else {
-                            editTextFields[counter].setBackgroundResource(R.color.red);
+                            letterBox[count].setBackgroundResource(R.color.red);
                         }
-                        counter++;
+                        count++;
                     }
                     letsInRow = 0;
                 }
             }
         } else {
-            keyboard(v);
-        }
-    }
-
-    private void keyboard(View v) {
-        Button button = (Button) v;
-        String buttonText = button.getText().toString();
-        if (!buttonText.isEmpty()) {
-            for (int i = 0; i < editTextFields.length; i++) {
-                EditText editText = editTextFields[i];
-                if (editText.getText().toString().isEmpty() && letsInRow < 5) {
-                    editText.setText(buttonText);
-                    if (i + 1 < editTextFields.length) {
-                        editTextFields[i + 1].requestFocus();
+            Button b = (Button) v;
+            String bText = b.getText().toString();
+            if (!bText.isEmpty()) {
+                for (int i = 0; i < letterBox.length; i++) {
+                    EditText letters = letterBox[i];
+                    if (letters.getText().toString().isEmpty() && letsInRow < 5) {
+                        letters.setText(bText);
+                        if (i + 1 < letterBox.length) {
+                            letterBox[i + 1].requestFocus();
+                        }
+                        letsInRow++;
+                        break;
                     }
-                    letsInRow++;
-                    break;
                 }
             }
-        }
-    }
 
-    private String getRowCharacters() {
-        StringBuilder rowCharacters = new StringBuilder();
-        for (int i = 0; i < letsInRow; i++) {
-            rowCharacters.append(editTextFields[i].getText().toString());
         }
-        return rowCharacters.toString();
     }
 }
 
