45faded4875dff0512e0d7199db5b716b3efb4ac
ardipi <ardendavis2@gmail.com>
2023-07-01 16:59:21 -0400

    player name and sprite in blackjack

--- app/src/main/java/com/example/cs2340Project/blackJackGame.java
+++ app/src/main/java/com/example/cs2340Project/blackJackGame.java
@@ -2,26 +2,35 @@ package com.example.cs2340Project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class blackJackGame extends AppCompatActivity{
     private Button restartButton;
     private Button homeButton;
     private TextView name;
 
+    private Player player;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.black_jack_game);
 
+        player = Player.getInstance();
+        selectLives();
+
         restartButton = findViewById(R.id.bj_restart_button);
-        name = findViewById(R.id.blackJackConfigScreen);
-        String username = getIntent().getStringExtra("keyname");
-        name.setText(username);
+//        name = findViewById(R.id.blackJackConfigScreen);
+//        String username = getIntent().getStringExtra("keyname");
+//        name.setText(username);
         restartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,4 +47,20 @@ public class blackJackGame extends AppCompatActivity{
             }
         });
     }
-}
+    @SuppressLint("ResourceAsColor")
+    private void selectLives() {
+        if(player.getPlayerLives() == 0){
+            Intent intent = new Intent(blackJackGame.this, gameOverScreen.class);
+            startActivity(intent);
+            finish();
+        }
+        TextView name = findViewById(R.id.playerDataName);
+        name.setText(player.getPlayerName());
+        //update the sprite image
+        ImageView sprite = findViewById(R.id.sprite);
+        player.setSpriteImage(sprite);
+        ImageView i1 = findViewById(R.id.life1);
+        ImageView i2 = findViewById(R.id.life2);
+        ImageView i3 = findViewById(R.id.life3);
+        player.setLives(i1,i2,i3);
+    }}
--- app/src/main/java/com/example/cs2340Project/blackJackInitialScreen.java
+++ app/src/main/java/com/example/cs2340Project/blackJackInitialScreen.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 
 public class blackJackInitialScreen extends AppCompatActivity{
     private Button prevButton;
     private Button continueButton;
-
+    private Player player;
     private EditText name;
     private Button add;
 
@@ -22,11 +24,13 @@ public class blackJackInitialScreen extends AppCompatActivity{
         name = findViewById(R.id.player_name);
         add = findViewById(R.id.bj_continue_button);
 
-
         continueButton = findViewById(R.id.bj_continue_button);
         continueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                player = Player.getInstance();
+                player.setPlayerName(name.getText().toString());
+                setSprite();
                 Intent intent = new Intent(blackJackInitialScreen.this, blackJackGame.class);
                 String username = name.getText().toString();
                 intent.putExtra("keyname", username);
@@ -44,5 +48,23 @@ public class blackJackInitialScreen extends AppCompatActivity{
             }
         });
 
+    }
+    private void setSprite(){
+        RadioButton pandaButton = findViewById(R.id.pandaButton);
+        RadioButton foxButton = findViewById(R.id.foxButton);
+        RadioButton turtleButton = findViewById(R.id.turtleButton);
+        ImageView sprite = findViewById(R.id.sprite);
+        if(pandaButton.isChecked()){
+            player.setPlayerSprite('p');
+            //sprite.setImageResource(R.drawable.panda);
+        }
+        else if(turtleButton.isChecked()){
+            player.setPlayerSprite('t');
+            //sprite.setImageResource(R.drawable.turtle);
+        }
+        else {
+            player.setPlayerSprite('f');
+            //sprite.setImageResource(R.drawable.fox);
+        }
     }
 }
