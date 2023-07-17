2b333986f53f236bb4fe7e87bf4ea237649b2b31
Charlie Lin <clin469@gatech.edu>
2023-07-16 22:54:03 -0400

    fixed gameOverScreen with score

--- app/src/main/java/com/example/cs2340Project/gameOverScreen.java
+++ app/src/main/java/com/example/cs2340Project/gameOverScreen.java
@@ -1,9 +1,11 @@
 package com.example.cs2340Project;
 
 import android.content.Intent;
+import android.net.PlatformVpnProfile;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,10 +19,13 @@ public class gameOverScreen extends AppCompatActivity {
         Button home = findViewById(R.id.go_home_button);
         Button restart = findViewById(R.id.go_restart_button);
 
+        Player player = Player.getInstance();
+
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(gameOverScreen.this, MainActivity.class);
+                player.setPlayerLives(3);
                 startActivity(intent);
                 finish();
             }
@@ -29,11 +34,21 @@ public class gameOverScreen extends AppCompatActivity {
         restart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(gameOverScreen.this, BlackJackInitialScreen.class);
+                Intent intent = new Intent(gameOverScreen.this, TicTacToeInitialScreen.class);
+                player.setPlayerLives(3);
                 startActivity(intent);
                 finish();
             }
         });
+
+        TextView latestScore = findViewById(R.id.go_game_score);
+        latestScore.setText("LATEST SCORE: " + player.getScore());
+        if (player.getScore() > player.getHighScore()) {
+            player.setHighScore(player.getScore());
+        }
+        TextView highScore = findViewById(R.id.go_game_high_score);
+        highScore.setText("HIGH SCORE: " + player.getHighScore());
+        player.setScore(0);
 
     }
 }
