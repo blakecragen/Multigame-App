bd99e2e692d5c99e066fb61e1b593fb65ae12360
Scott Williams <scott.williams@epfl.ch>
2023-07-21 16:15:09 -0400

    Appeasing Git

--- app/src/main/java/com/example/cs2340Project/gameOverScreen.java
+++ app/src/main/java/com/example/cs2340Project/gameOverScreen.java
@@ -1,15 +1,13 @@
 package com.example.cs2340Project;
 
 import android.content.Intent;
-import android.net.PlatformVpnProfile;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class gameOverScreen extends AppCompatActivity {
+public class GameOverScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +17,10 @@ public class gameOverScreen extends AppCompatActivity {
         Button home = findViewById(R.id.go_home_button);
         Button restart = findViewById(R.id.go_restart_button);
 
-        Player player = Player.getInstance();
-
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(gameOverScreen.this, MainActivity.class);
-                player.setPlayerLives(3);
+                Intent intent = new Intent(GameOverScreen.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
@@ -34,21 +29,11 @@ public class gameOverScreen extends AppCompatActivity {
         restart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(gameOverScreen.this, TicTacToeInitialScreen.class);
-                player.setPlayerLives(3);
+                Intent intent = new Intent(GameOverScreen.this, BlackJackInitialScreen.class);
                 startActivity(intent);
                 finish();
             }
         });
-
-        TextView latestScore = findViewById(R.id.go_game_score);
-        latestScore.setText("LATEST SCORE: " + player.getScore());
-        if (player.getScore() > player.getHighScore()) {
-            player.setHighScore(player.getScore());
-        }
-        TextView highScore = findViewById(R.id.go_game_high_score);
-        highScore.setText("HIGH SCORE: " + player.getHighScore());
-        player.setScore(0);
 
     }
 }
