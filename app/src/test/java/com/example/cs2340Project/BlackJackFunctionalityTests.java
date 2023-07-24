3b909178759fe83a689b5fbb70e61bd7fcd79f44
Blake Cragen <113573797+blakecragen@users.noreply.github.com>
2023-07-23 20:54:04 -0400

    Update BlackJackFunctionalityTests.java
    Fixed a functionality test

--- app/src/test/java/com/example/cs2340Project/BlackJackFunctionalityTests.java
+++ app/src/test/java/com/example/cs2340Project/BlackJackFunctionalityTests.java
@@ -72,52 +72,6 @@ public class BlackJackFunctionalityTests {
         assertEquals(test.size(), 2);
     }
 
-    @Test
-    public void testFunctionality() throws IOException {
-        Scanner scan = new Scanner(System.in);
-        BlackJackDeck myDeck = new BlackJackDeck();
-        BlackJackPlayer me = new BlackJackPlayer();
-        BlackJackDealer dealer = new BlackJackDealer();
-        int money = 500;
-        int bet = 0;
-        String cont = "y";
-        String hit = "n";
-        while ("y".equals(cont)) {
-            bet = scan.nextInt();
-            dealer.hit(myDeck);
-            me.hit(myDeck);
-            dealer.hit(myDeck);
-            me.hit(myDeck);
-            while (me.getHandSum() < 21 & hit.equals("y")) {
-                Runtime.getRuntime().exec("cls");
-                dealer.printShownCard();
-                System.out.println();
-                me.printHand();
-                System.out.println();
-                System.out.println("Hit?");
-                hit = scan.nextLine();
-                if ("y".equals(hit)) {
-                    me.hit(myDeck);
-                }
-            }
-            dealer.hit(myDeck);
-            Runtime.getRuntime().exec("cls");
-            dealer.printHand();
-            me.printHand();
-            int won = dealer.playerWin(me);
-            if (won == 2) {
-                System.out.println("You won!");
-            } else if (won == 1) {
-                System.out.println("Tie");
-            } else {
-                System.out.println("Loss :(");
-            }
-            System.out.println();
-            System.out.println("Play again?");
-            cont = scan.nextLine();
-            Runtime.getRuntime().exec("cls");
-        }
-    }
 
     @Test
     public void testPlayerHandSum() {
